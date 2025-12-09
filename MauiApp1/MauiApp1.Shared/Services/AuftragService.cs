4d07924e2ab23f27df6ab32964f9b0c02d22e38c
Fabio Leuthold <fabio.leuthold@hotmail.ch>
2025-12-09 20:22:51 +0100

    added whole maui project that does similar things like the webforms app (content wise actually exactly the same)

---
+++ MauiApp1/MauiApp1.Shared/Services/AuftragService.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Data.SqlClient;
+
+namespace MauiApp1.Shared.Services
+{
+    internal static class AuftragService
+    {
+        public static List<Drucker> GetAllDrucker()
+        {
+            var druckerListe = new List<Drucker>();
+            using (var conn = new SqlConnection("Data Source=.;Initial Catalog=wawi;Integrated Security=True;TrustServerCertificate=True"))
+            {
+                conn.Open();
+                using (var cmd = new SqlCommand("SELECT * FROM Drucker", conn))
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var drucker = new Drucker
+                        {
+                            Id = reader.GetInt32(0),
+                            Name = reader.GetString(1)
+                        };
+                        druckerListe.Add(drucker);
+                    }
+                }
+            }
+            return druckerListe;
+        }
+        public static List<Auftrag>GetByDrucker(int druckerId)
+        {
+            var auftragListe = new List<Auftrag>();
+            using (var conn = new SqlConnection("Data Source=.;Initial Catalog=wawi;Integrated Security=True;TrustServerCertificate=True"))
+            {
+                conn.Open();
+                using (var cmd = new SqlCommand("SELECT * FROM Auftrag where DruckerId = @DruckerId", conn))
+                {
+                    cmd.Parameters.AddWithValue("@DruckerId", druckerId);
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var drucker = new Auftrag
+                            {
+                                Id = reader.GetInt32(0),
+                                DruckerId = reader.GetInt32(1),
+                                ArtikelId = reader.GetInt32(2),
+                                Status = reader.GetString(3)
+                            };
+                            auftragListe.Add(drucker);
+                        }
+                    }
+                }
+                
+            }
+            return auftragListe;
+        }
+    }
+
+    internal class Drucker
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    internal class Auftrag
+    {
+        public int Id { get; set; }
+        public int DruckerId { get; set; }
+        public int ArtikelId { get; set; }
+        public string Status { get; set; }
+    }
+}
--- WebApplication1/README.md
+++ WebApplication1/README.md
@@ -1,11 +1,16 @@
-cd C:\Program Files\IIS Express\
+cd C:\\Program Files\\IIS Express\\
 
-iisexpress.exe /path:C:\Windows\Microsoft.NET\Framework\v4.0.30319\ASP.NETWebAdminFiles /vpath:"/ASP.NETWebAdminFiles" /port:8082 /clr:4.0 /ntlm
+iisexpress.exe /path:C:\\Windows\\Microsoft.NET\\Framework\\v4.0.30319\\ASP.NETWebAdminFiles /vpath:"/ASP.NETWebAdminFiles" /port:8082 /clr:4.0 /ntlm
 
-http://localhost:8082/asp.netwebadminfiles/default.aspx?applicationPhysicalPath=C:\Users\fabio\source\repos\WebApplication1\WebApplication1\&applicationUrl=/
+http://localhost:8082/asp.netwebadminfiles/default.aspx?applicationPhysicalPath=C:\\Users\\fabio\\source\\repos\\WebApplication1\\WebApplication1\&applicationUrl=/
 
 
-C:\Users\fabio\source\repos\WebApplication1\WebApplication1
 
-http://localhost/webadmin/default.aspx?applicationPhysicalPath=C:\derdeploy\app.publish\&applicationUrl=/
+C:\\Users\\fabio\\source\\repos\\WebApplication1\\WebApplication1
+
+http://localhost/webadmin/default.aspx?applicationPhysicalPath=C:\\derdeploy\\app.publish\&applicationUrl=/
+
+
+
+http://localhost/webadmin/?applicationPhysicalPath=C:\\derdeploy\\app.publish\&applicationUrl=/
 
